7ec7e33dfcf37fad978f35b00e1e8ea04e65b094
dmitry-sharashkin <faustfbs@mail.ru>
2021-12-29 02:13:41 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
 import React from 'react'
-import axios from "axios";
+import Sidebar from "./components/sidebar";
+import Box from "@mui/material/Box";
+
 
 function App() {
 
-    const options = {
-        method: 'GET',
-        url: 'https://coinranking1.p.rapidapi.com/coin/1',
-        headers: {
-            'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
-            'x-rapidapi-key': 'c4cb59fad1mshbf9a6aec398f84bp10882ejsnf417ab1af47d'
-        }
-    };
-
-    axios.request(options).then(function (response) {
-        console.log(response.data);
-    }).catch(function (error) {
-        console.error(error);
-    });
 
     return (
-        <div className="App">
-            <h1>Crypto</h1>
-        </div>
+        <Box sx={{ display: 'flex',  }}>
+            <Sidebar/>
+        </Box>
     );
 }
 
---
+++ src/components/sidebar.jsx
@@ -0,0 +1,104 @@
+import React from 'react';
+import Drawer from "@mui/material/Drawer";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import Divider from "@mui/material/Divider";
+import Toolbar from "@mui/material/Toolbar";
+import CssBaseline from "@mui/material/CssBaseline";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+
+import {createTheme,ThemeProvider } from "@mui/system";
+
+const theme = createTheme({
+    palette: {
+        background: {
+            paper: '#ff0000',
+        },
+        text: {
+            primary: '#173A5E',
+            secondary: '#46505A',
+        },
+        action: {
+            active: '#001E3C',
+        },
+        success: {
+            dark: '#009688',
+        },
+    },
+});
+
+
+const drawerWidth = 240;
+
+const Sidebar = () => {
+    return (<>
+            <CssBaseline/>
+
+            <Drawer
+                sx={{
+                    width: drawerWidth,
+                    flexShrink: 0,
+                    '& .MuiDrawer-paper': {
+                        width: drawerWidth,
+                        boxSizing: 'border-box',
+                    },
+                }}
+                variant="permanent"
+                anchor="left"
+            >
+                <Toolbar/>
+                <Divider/>
+                <List  >
+                    {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                        <ListItem button key={text}>
+                            <ListItemIcon sx={{color:  'primary.dark'}}>
+                                {index % 2 === 0 ? <InboxIcon /> : <MailIcon/>}
+                            </ListItemIcon>
+                            <ListItemText primary={text}/>
+                        </ListItem>
+                    ))}
+                </List>
+                <Divider/>
+                <List>
+                    {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                        <ListItem button key={text}>
+                            <ListItemIcon>
+                                {index % 2 === 0 ? <InboxIcon/> : <MailIcon/>}
+                            </ListItemIcon>
+                            <ListItemText primary={text}/>
+                        </ListItem>
+                    ))}
+                </List>
+            </Drawer>
+            <Box mt={10}
+                component="main"
+                sx={{flexGrow: 1, bgcolor: 'background.default', p: 3, color:  'primary.main',border:2, boxShadow:10}}
+            >
+                <Toolbar/>
+                <Typography paragraph>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
+                    enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
+                    imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
+                    Convallis convallis tellus id interdum velit laoreet id donec ultrices.
+                    Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
+                    adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra
+                    nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum
+                    leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis
+                    feugiat vivamus at augue. At augue eget arcu dictum varius duis at
+                    consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
+                    sapien faucibus et molestie ac.
+                </Typography>
+
+            </Box>
+        </>
+
+    )
+};
+
+export default Sidebar
